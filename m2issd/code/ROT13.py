3c14e76bd9320410b2d8c35d94edc6123d9e3244
Philippe Truillet <Philippe.Truillet@irit.fr>
2021-03-23 18:11:43 +0100

    Add files via upload

--- m2issd/code/ROT13.py
+++ m2issd/code/ROT13.py
@@ -1,9 +1,12 @@
-﻿# code de Caear
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Exercice 1 - code de Caesar
 
 # fonction d'encodage
 def encode(texte):
     # definir le dictionnaire d'encodage
-    rot13 = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a','o':'b','p':'c','q':'d','r':'e','s':'f','t':'g','u':'h','v':'i','w':'j','x':'k','y':'l', 'z':'m'}
+    rot13 = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a','o':'b','p':'c','q':'d','r':'e','s':'f','t':'g','u':'h','v':'i','w':'j','x':'k','y':'l', 'z':'m', ' ':' '}
     # coder les lettres
     texte_encode=''
     for i in range(len(texte)):
@@ -11,5 +14,9 @@ def encode(texte):
     return(texte_encode)
 
 # programme principal
-txt= input("Entrez le texte à encoder: ")
-print("texte encode : " + encode(txt))
+def main():
+    txt= input("Entrez le texte à encoder: ")
+    print("texte encode : " + encode(txt))
+
+if __name__ == '__main__':
+	main()
